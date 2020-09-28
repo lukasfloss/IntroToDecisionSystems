1c7fc7b2c034a2b9b9f56d5fc99abba6f355ad44
Lukas Floß <lukas.floss@htlvb.at>
2020-09-28 16:15:31 +0200

    Implement Utils.GetDistance

--- DecisionSystems/TSP/Utils.cs
+++ DecisionSystems/TSP/Utils.cs
@@ -6,9 +6,29 @@ namespace DecisionSystems.TSP
 {
     public static class Utils
     {
+        public static double GetDistance(Location a, Location b)
+        {
+            var dx = a.X - b.X;
+            var dy = a.Y - b.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
         public static double GetDistance(IReadOnlyCollection<int> solution, IReadOnlyList<Location> cities)
         {
-            return 0;
+            var previousCityIndex = solution.Last();
+            var distance = 0.0;
+
+            // Iterate through the list.
+            foreach (var cityIndex in solution)
+            {
+                var previousCity = cities[previousCityIndex - 1];
+                var city = cities[cityIndex - 1];
+                distance += GetDistance(previousCity, city);
+                previousCityIndex = cityIndex;
+            }
+            return distance;
+           
+            
         }
     }
 }
